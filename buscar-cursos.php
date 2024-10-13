027d17e12c88ea9893b4e3d996bff029a5d6c954
Wallace Henrique <144370869+wollihenrique@users.noreply.github.com>
2024-10-13 09:15:22 -0300

    First commit

---
+++ buscar-cursos.php
@@ -0,0 +1,22 @@
+<?php
+
+require 'vendor/autoload.php';
+require 'src/Buscador.php';
+
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+use wollihenrique\BuscadorCursos\Buscador;
+
+$client = new Client
+([
+    'verify' => false,
+    'base_uri' => 'https://www.alura.com.br/'
+]);
+$crawler = new Crawler();
+
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
+foreach ($cursos as $curso){
+    echo exibeMensagem($curso);
+}
